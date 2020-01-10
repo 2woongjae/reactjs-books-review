9c3251c51da9a5ac92aec66bf0d174700cc3a5ac
mark <mark@protopie.io>
2020-01-10 20:03:10 +0900

    after week3

---
+++ src/components/SigninBg.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Col } from "antd";
+import bg from "../images/bg_signin.png";
+
+const SigninBg = () => (
+  <Col span={12}>
+    <img src={bg} style={{ width: "100%" }} alt="Signin" />
+  </Col>
+);
+
+export default SigninBg;
---
+++ src/pages/Signin.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import styled from "styled-components";
+import { Row, Col } from "antd";
+import SigninBg from "../components/SigninBg";
+
+const Page = styled(Row).attrs(() => ({
+  type: "flex",
+  align: "middle"
+}))`
+  height: 100vh;
+`;
+
+const Title = styled.div`
+  text-align: center;
+  font-family: Roboto;
+  font-size: 40px;
+  font-weight: bold;
+  color: #642828;
+  text-transform: uppercase;
+`;
+
+const SubTitle = styled.div`
+  text-align: center;
+  font-family: Roboto;
+  font-size: 27px;
+  font-weight: bold;
+  text-transform: uppercase;
+`;
+
+const Underline = styled.div`
+  width: 200px;
+  height: 8px;
+  margin-right: auto;
+  margin-left: auto;
+  margin-top: 20px;
+  background: linear-gradient(to right, #803b32, #ddb49b);
+`;
+
+const Contents = styled(Row).attrs(() => ({
+  type: "flex"
+}))`
+  margin-top: 50px;
+  background-color: #f3f7f8;
+  margin-left: auto;
+  margin-right: auto;
+  width: 800px;
+`;
+
+const Signin = () => (
+  <Page>
+    <Col span={24}>
+      <Title>MARKTUBE REVIEW SERVICE FOR BOOKS</Title>
+      <SubTitle>PLEASE SHARE YOUR OPINION ON WEB DEVELOPMENT BOOKS.</SubTitle>
+      <Underline />
+      <Contents>
+        <SigninBg />
+      </Contents>
+    </Col>
+  </Page>
+);
+
+export default Signin;
